a0261bc668a9f21d1122c015268b455c24c06909
Deepak Marneni <marnenideepak4@gmail.com>
2023-01-20 00:25:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -152,8 +152,34 @@ function safariIniti() {
 
 function askSafari(){
 debugger
-safariIniti();
+   if ('safari' in window && 'pushNotification' in window.safari) {
+        var permissionData = window.safari.pushNotification.permission('web.com.example.domain');
+        checkRemotePermission(permissionData);
+    }
+//safariIniti();
 }
+
+var checkRemotePermission = function (permissionData) {
+    if (permissionData.permission === 'default') {
+        // This is a new web service URL and its validity is unknown.
+        window.safari.pushNotification.requestPermission(
+            'https://push-demo-lgmswqbdha-uc.a.run.app/push-api', // The web service URL.
+            'web.com.imiconnect.safari.safari15push',     // The Website Push ID.
+            {}, // Data that you choose to send to your server to help you identify the user.
+            checkRemotePermission         // The callback function.
+        );
+    }
+    else if (permissionData.permission === 'denied') {
+        // The user said no.
+    }
+    else if (permissionData.permission === 'granted') {
+        // The web service URL is a valid push provider, and the user said yes.
+        // permissionData.deviceToken is now available to use.
+    }
+};
+
+
+
 
 /*
  * Call relevant methods.
